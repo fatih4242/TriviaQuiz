514541e22cd77024e45182a5eff5472c9dfabccd
fatih4242 <fatih16@live.nl>
2022-10-08 11:48:57 +0300

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Fatih Toker on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
